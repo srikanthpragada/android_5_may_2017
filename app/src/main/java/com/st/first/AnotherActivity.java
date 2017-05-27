36b049f6d85fa02802d8b00ede068b3d83210997
srikanthpragada <srikanthpragada@gmail.com>
2017-05-27 21:04:15 +0530

    27-MAY

--- app/src/main/java/com/st/first/AnotherActivity.java
+++ app/src/main/java/com/st/first/AnotherActivity.java
@@ -1,7 +1,9 @@
 package com.st.first;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class AnotherActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class AnotherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another);
+
+        Intent intent = getIntent();
+
+        Log.d("First",intent.getData().toString());
     }
 }
--- app/src/main/java/com/st/first/IntentDemoActivity.java
+++ app/src/main/java/com/st/first/IntentDemoActivity.java
@@ -2,13 +2,15 @@ package com.st.first;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class IntentDemoActivity extends Activity {
 
-    EditText editAction, editCategory;
+    EditText editAction, editCategory,editData, editMIME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +19,34 @@ public class IntentDemoActivity extends Activity {
 
         editAction = (EditText) findViewById(R.id.editAction);
         editCategory = (EditText) findViewById(R.id.editCategory);
+        editData = (EditText) findViewById(R.id.editData);
+        editMIME = (EditText) findViewById(R.id.editMIME);
     }
 
     public void invoke(View v) {
 
         Intent intent = new Intent();
         if ( editAction.getText().toString().trim().length() != 0 )
-             intent.setAction( editAction.getText().toString());
-        startActivity(intent);
+            intent.setAction( editAction.getText().toString());
+
+        if ( editCategory.getText().toString().trim().length() != 0)
+             intent.addCategory(editCategory.getText().toString());
+
+
+        if ( editData.getText().toString().trim().length() != 0)
+            intent.setData(Uri.parse(editData.getText().toString()));
+
+        if ( editMIME.getText().toString().trim().length() != 0)
+            intent.setType(editMIME.getText().toString());
+
+        try {
+            startActivity(intent);
+        }
+        catch(Exception ex) {
+            Log.d("First", ex.getMessage());
+        }
 
     }
+
+
 }
